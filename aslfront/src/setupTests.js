a0715fdc62c570194f1e88ece68f6336182257cd
Liuyuyuan74 <95260740+Liuyuyuan74@users.noreply.github.com>
2024-03-20 16:23:08 -0400

    debug test

--- aslfront/src/setupTests.js
+++ aslfront/src/setupTests.js
@@ -3,9 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-// global.fetch = jest.fn(() =>
-//   Promise.resolve({
-//     json: () => Promise.resolve({ url: "https://preSignedImageURL.com" }),
-//   })
-// );
